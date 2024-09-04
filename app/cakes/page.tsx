9c751b0a117d74fe972bd892e9a9d7a0c575f3db
Karla Pupačić <92401487+kpupacic@users.noreply.github.com>
2024-09-04 11:34:22 +0200

    mobile version + design updates

--- app/cakes/page.tsx
+++ app/cakes/page.tsx
@@ -1,27 +1,3 @@
-// import SearchParams from "@/app/blog/page";
-// import { FC } from "react";
-// import { RecipeCard } from "../page2";
-// import contentfulService from "@/lib/contentfulClient";
-
-// const CakesPage: FC<SearchParams> = async ({ searchParams }) => {
-//   const recipes = await contentfulService.getAllRecipes();
-//   const filteredRecipes = recipes.filter(recipe => recipe.tags.includes('cake'));
-
-//   return (
-//     <main>
-//       <ul className="cards gap-5">
-//         {filteredRecipes.sort((a, b) => a.name.localeCompare(b.name)).map((recipe) => (
-//           <li key={recipe.id}>
-//             <RecipeCard {...recipe} />
-//           </li>
-//         ))}
-//       </ul>
-//     </main>
-//   );
-// };
-
-// export default CakesPage;
-
 import { FC } from "react";
 import { RecipeCard } from "../page2";
 import contentfulService from "@/lib/contentfulClient";
@@ -33,6 +9,10 @@ const CakesPage: FC<{ searchParams: SearchParams }> = async ({ searchParams }) =
 
   return (
     <main>
+      <div className="section-description">
+        <h1>Cakes</h1>
+        <p>A slice of heaven in every recipe — find them all here.</p>
+      </div>
       <ul className="cards gap-5">
         {filteredRecipes.sort((a, b) => a.name.localeCompare(b.name)).map((recipe) => (
           <li key={recipe.id}>
